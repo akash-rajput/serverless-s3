27142d9a93c5d0bea932c19e89aeacad119fbe4c
akash-rajput <devakashrajput@gmail.com>
2020-11-02 17:18:59 +0530

    Added master route component

---
+++ src/RepoList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { octokit } from './App';
+
+function RepoList() {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    octokit.request('GET /orgs/:org/repos', {
+      org: 'octokit',
+    }).then(data => setRepos(data.data));
+    
+  }, []);
+
+  return (
+    <>
+      {repos.map((repo) => (
+        <div key={repo.id} className="repo-list-item">
+          <Link to={`/repository/${repo.id}`}>{repo.full_name}</Link>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default RepoList;
